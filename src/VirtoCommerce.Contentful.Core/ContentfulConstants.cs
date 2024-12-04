67c97b14cfd8e401efcdf0c9ffb9fc003078f33b
Basil Kotov <basilkot@gmail.com>
2024-12-04 18:24:34 +0000

    change permissions values

--- src/VirtoCommerce.Contentful.Core/ContentfulConstants.cs
+++ src/VirtoCommerce.Contentful.Core/ContentfulConstants.cs
@@ -6,11 +6,11 @@ public static class ContentfulConstants
     {
         public static class Permissions
         {
-            public const string Read = "content:read",
-                Create = "content:create",
-                Access = "content:access",
-                Update = "content:update",
-                Delete = "content:delete";
+            public const string Read = "contentful:read";
+            public const string Create = "content:create";
+            public const string Access = "content:access";
+            public const string Update = "content:update";
+            public const string Delete = "content:delete";
 
             public static readonly string[] AllPermissions = [Read, Create, Access, Update, Delete];
         }
